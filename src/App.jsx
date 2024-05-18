e27e5d5370a514fe4b48800b23775b00d8186441
Harshiiet Negi <negeek7@gmail.com>
2024-05-18 12:16:28 +0530

    video url input

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import { useState } from 'react'
 import './App.css'
+import styles from '../styles/app.module.css'
+import VideoUrlInput from './components/Inputs/VideoUrlInput'
 
 function App() {
+
+  const handleVideoUrl = (url) => {
+    console.log(url, "VIDEO URL")
+  }
+
   return (
-    <div style={{border:"1px solid red", textAlign:"center"}}>
-      <h1>Video Caption</h1>
-    </div>
+    <>
+      <h1 className={styles.heading}>Video Caption</h1>
+      <VideoUrlInput handleVideoUrl={handleVideoUrl}/>
+    </>
   )
 }
 
---
+++ src/components/Inputs/VideoUrlInput.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react';
+import styles from '../../../styles/videourlinput.module.css'
+
+function VideoUrlInput({handleVideoUrl}) {
+
+    const [url, setUrl] = useState('')
+
+    useEffect(() => {
+        if(url){
+            handleVideoUrl(url)
+        }
+    }, [url])
+
+    const handleUrlChange = (e) => {
+        let value = e.target.value
+        setUrl(value)
+    }
+
+    return (
+        <div className={styles.container}>
+            <input
+                type="text"
+                value={url}
+                onChange={handleUrlChange}
+                placeholder='Enter Video URL...'
+                className={styles.input}
+            />
+        </div>
+    )
+}
+
+export default VideoUrlInput
