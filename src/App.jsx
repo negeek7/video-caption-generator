d750ffefaf67eee1f84c604cdfc27b022aedb43c
Harshiiet Negi <negeek7@gmail.com>
2024-05-18 12:00:38 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div style={{border:"1px solid red", textAlign:"center"}}>
+      <h1>Video Caption</h1>
+    </div>
+  )
+}
+
+export default App
